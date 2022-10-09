1d0832f861d9f6dd7a2c12c36e2d2abcccf88173
Clyde <noneclyde@gmail.com>
2022-10-09 16:09:01 +0300

    Add new class with showMenu function

---
+++ Habit-Tracker/Habit-Tracker.cs
@@ -0,0 +1,18 @@
+﻿
+internal class Habit_Tracker
+{
+    private Database database = new Database();
+
+    internal void showMenu()
+    {
+        Console.Clear();
+        Console.WriteLine("\nMain Menu");
+        Console.WriteLine("\nWhat would you like to do:");
+        Console.WriteLine("\nType 0 To Close Application");
+        Console.WriteLine("Type 1 To View All Records");
+        Console.WriteLine("Type 2 To Insert Record");
+        Console.WriteLine("Type 3 To Delete Record");
+        Console.WriteLine("Type 4 To Update Record");
+    }
+}
+
