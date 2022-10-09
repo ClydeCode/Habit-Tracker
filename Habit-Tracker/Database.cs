9f213d41b3cdfbd32af32268fb9747bffd5d33ed
Clyde <noneclyde@gmail.com>
2022-10-09 14:25:33 +0300

    Initial Setup

---
+++ Habit-Tracker/Database.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Data.Sqlite;
+
+class Database
+{
+    private string connectionString = @"Data Source=habit-Tracker.db";
+
+    internal void CreateDatabase()
+    {
+        using (var connection = new SqliteConnection(connectionString))
+        {
+            using (var command = connection.CreateCommand())
+            {
+                connection.Open();
+
+                command.CommandText =
+                    @"CREATE TABLE IF NOT EXISTS yourHabit (
+						Id INTEGER PRIMARY KEY AUTOINCREMENT
+						Date TEXT
+						Quantity INTEGER
+					)";
+
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
