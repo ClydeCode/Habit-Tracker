7e0e306e10dd412a7e77b745df0fda5e8f4caea9
Clyde <noneclyde@gmail.com>
2022-10-09 17:28:13 +0300

    Update setup

--- Habit-Tracker/Program.cs
+++ Habit-Tracker/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿Habit_Tracker habit_tracker = new Habit_Tracker();
+
+while (true)
+{
+    habit_tracker.showMenu();
+
+    habit_tracker.navigate(Helpers.getInt());
+
+    Console.WriteLine("\nPress ENTER to Continue...");
+    Console.ReadLine();
+}
