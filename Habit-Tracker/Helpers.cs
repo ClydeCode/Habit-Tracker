e6818c639fc433e155d8471d1a841497085959ef
Clyde <noneclyde@gmail.com>
2022-10-09 17:21:31 +0300

    Add getInt and getDate functions

---
+++ Habit-Tracker/Helpers.cs
@@ -0,0 +1,27 @@
+﻿
+class Helpers
+{
+    internal static int getInt()
+    {
+        int number = -1;
+
+        while (number < 0)
+        {
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out int proper_input)) { number = proper_input; }
+            else
+            {
+                Console.WriteLine("Wrong Input! Try again...");
+            }
+        }
+        return number;
+    }
+
+    internal static DateTime getDate()
+    {
+        DateTime date = DateTime.Now;
+        return date.Date;
+    }
+}
+
